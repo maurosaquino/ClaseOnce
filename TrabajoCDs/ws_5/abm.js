06e2286743de7f8af87a724c988ec6a8d4036eb2
maurosaquino <mauro.s.aquino@gmail.com>
2016-11-01 21:36:47 -0300

    Clase Once WebService

---
+++ TrabajoCDs/ws_5/abm.js
@@ -0,0 +1,43 @@
+function ingresar(){
+
+	var n =  $("#cantante").val();
+	var a =  $("#ano").val();
+	var t =  $("#titulo").val();
+
+	$.ajax({url:"nexo.php",type:"post",data:{accion:"ingresar",nombre:n,ano:a,titulo:t}}).then(
+	function(exito){
+
+		mostrar();
+
+
+
+	},function(error){
+
+		$("#contenedor").html(error);
+			
+
+	});
+
+
+}
+
+
+
+function mostrar(){
+
+	$.ajax({url:"nexo.php",type:"post",data:{accion:"mostrar"}}).then(
+	function(exito){
+
+		$("#contenedor").html(exito);
+
+
+
+	},function(error){
+
+		$("#contenedor").html(error);
+			
+
+	});
+
+
+}
